877cb5c1f7142ebf74e7e920d568b9b1de7330fd
Oscar Ablinger <dev.ablinger@gmail.com>
2021-01-16 20:57:55 +0100

    feat: parse logs

--- main.js
+++ main.js
@@ -102,7 +102,49 @@ function askForUserInput() {
 }
 
 
+const eventStartRegex = /^(\d{1,2})\/(\d{1,2})\/(\d{1,2}), (\d{2}):(\d{2}) - /m
+const messageStartRegex = /^(\d{1,2})\/(\d{1,2})\/(\d{1,2}), (\d{2}):(\d{2}) - ([^:]+): (.+)$/m
+
+const messageType = {
+    message: "message",
+    notification: "notification"
+}
+
+function parseLogs({ options, chatLog, mediaFiles }) {
+    const logLines = chatLog.split('\n')
+    const events = []
+
+    for (line of logLines) {
+        if (!eventStartRegex.test(line)) {
+            // not an event start -> add to previous event (multi-line)
+            events[events.length - 1].text += '\n' + line
+        } else {
+            const match = line.match(messageStartRegex)
+            
+            if (match) {
+                // message start -> new message
+                [_, mo, d, y, h, mi, sender, text] = match
+                events.push({
+                    type: messageType.message,
+                    time: new Date(y, mo, d, h, mi, 0, 0),
+                    sender,
+                    text
+                })
+            } else {
+                // notification (e.g. X created group)
+                events.push({
+                    type: messageType.notification,
+                    text: line.slice(line.indexOf('-') + 2) // +2 to catch - and the space afterwards
+                })
+            }
+        }
+    }
+
+    return { events, options, mediaFiles }
+}
+
 askForUserInput()
+    .then(parseLogs)
     .catch(reason => {}) // we don't have to do anything here – this is to avoid the error when you have an uncaught promise
 
 })()
